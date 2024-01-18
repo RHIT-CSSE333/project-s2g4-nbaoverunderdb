2a2b654980b3c5ffa81110860fcc8eb871d39ed5
KP <116210694+rhit-pengkx@users.noreply.github.com>
2024-01-18 17:31:02 -0500

    Add files via upload

---
+++ InsertTeam.sql
@@ -0,0 +1,20 @@
+USE NBAOverUnderDB
+GO
+CREATE PROCEDURE InsertTeam (@Name VARCHAR(255))
+AS
+BEGIN
+	IF(@Name IS NULL)
+	BEGIN
+		PRINT("ERROR: Name is null")
+		RETURN 1
+	END
+	IF(EXISTS(SELECT * FROM [Team] WHERE Name = @Name))
+	BEGIN
+		PRINT("ERROR: Team already exists")
+		RETURN 2
+	END
+	INSERT INTO Team (Name)
+	VALUES(@Name)
+	PRINT("SUCCESSFUL INSERT")
+	RETURN 0
+END
