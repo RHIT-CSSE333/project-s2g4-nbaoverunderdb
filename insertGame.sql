44113bff1d110e334817f2b75d6e4bce851e6d1a
rhit-davidj <123593285+rhit-davidj@users.noreply.github.com>
2024-01-18 17:31:13 -0500

    Added insert game sproc

---
+++ insertGame.sql
@@ -0,0 +1,29 @@
+USE NBAOverUnderDB
+Go
+
+CREATE PROCEDURE InsertGameByTeamNames(@HomeTeamName VARCHAR(255), @AwayTeamName VARCHAR(255))
+AS
+BEGIN
+    DECLARE @HomeTeamID INT;
+    DECLARE @AwayTeamID INT;
+
+    SELECT @HomeTeamID = ID FROM Team WHERE Name = @HomeTeamName;
+
+	IF @HomeTeamID IS NULL
+	BEGIN
+		PRINT 'Home team ' + @HomeTeamName + ' not found'
+		Return 1
+	END
+
+    SELECT @AwayTeamID = ID FROM Team WHERE Name = @AwayTeamName;
+
+	IF @AwayTeamName IS NULL
+	BEGIN
+		PRINT 'Away team ' + @AwayTeamName + ' not found'
+		Return 1
+	END
+
+    INSERT INTO Game ([Home Team ID], [Away Team ID])
+    VALUES (@HomeTeamID, @AwayTeamID);
+	Return 0
+END;
