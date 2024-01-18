e3edc2f33873b8a09b4048be7e690c5519919320
rhit-davidj <davidj@rose-hulman.edu>
2024-01-18 17:47:12 -0500

    Slight html changes, added dependancies to venv

--- dataparser.py
+++ dataparser.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import pyodbc
 
-server = 'your_server'
+server = 'golem.csse.rose-hulman.edu'
 database = 'NBAOverUnderDB'
 username = 'nbaoverunderuser'
 password = 'NBAPassword123'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
                       server+';DATABASE='+database+';UID='+username+';PWD=' + password)
 
-csv_file = 'path/to/your/csvfile.csv'
+csv_file = 'nbadata.csv'
 
-df = pd.read_csv(nbadata.csv)
+df = pd.read_csv(csv_file)
 
 for index, row in df.iterrows():
     home_team_name = row['HomeTeam']
